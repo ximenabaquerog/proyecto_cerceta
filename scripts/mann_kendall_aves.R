28a76b897271063db24817a95beb07779b7cf92f
ximenabaquerog <ximenabaquerog@gmail.com>
2026-03-17 17:51:26 +0100

    Update mann_kendall_aves.R
    codigo mann kendall

--- scripts/mann_kendall_aves.R
+++ scripts/mann_kendall_aves.R
@@ -1,2 +1,25 @@
 ################################ ANALISIS MANN-KENDALL TENDENCIAS DE AVES #################################
 
+#https://www.geeksforgeeks.org/r-machine-learning/how-to-perform-a-mann-kendall-trend-test-in-r/
+
+#Para llevarloa cabo necesito una tabla con una columande anos y otra columna de datos de aves (abunda y otra columna con riqueza), seran 2 mann kendall
+
+#Instalo y cargo paquetes necesarios para Mann-Kendall
+install.packages("Kendall")
+library(Kendall)
+
+#Cargo variables del Mann-Kendall
+tend_riqueza <- read.csv("")
+
+tend_abundancia <- read.csv("")
+
+#Llevo a cabo los tests
+MannKendall(x)
+
+#Visualizamos
+#Plot the time series data
+plot(x)
+#Add a smooth line to visualize the trend 
+lines(lowess(time(x),x), col='blue')
+
+
